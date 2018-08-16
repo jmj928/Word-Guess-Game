f4c36f1714c8fdf049eeb4fe741fb1d916933200
jmj928 <jmj928@vt.edu>
2018-08-15 22:19:34 -0400

    initial screen complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,86 @@
 
-
+window.onload = function () {
     //global varibales
         var wins = 0;
         var losses = 0;
         var guessesLeft = 10;
         var guessedLetters = [];
-        var computerChoices = ["madonna", "cher"];
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        var computerChoices = ["carrie", "jaws", "insidious","the shining"];
+        var gameStarted = false;
+        var wordArray =[];
+        var userGuessedVisisbleWord = [];
+        var initialScreen;
+        var selectedWord;
+       
+        resetToBeginning();
 
-        var initialScreen = computerGuess.replace(/[a-z]/gi, '_ ')
-        var html =
-        "<h2> Press any key to get started </h2>" +
-        "<p>" + initialScreen + "</p>";
-   
-            document.querySelector("#game").innerHTML = html;
-      
+        function resetToBeginning(){
+            guessesLeft = 10;
+            gameStarted = false;
+
+            //clear arrays
+            guessedLetters =[];
+            wordArray =[];
+            userGuessedVisisbleWord = [];
+
+            //get random word
+            selectedWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+            //turn word into array
+            wordArray = selectedWord.split();
+
+        
+            //make word to be displayed
+            for (var i = 0; i < selectedWord.length; i++)
+            {
+                if(selectedWord[i] === " ")
+                {
+                    userGuessedVisisbleWord.push(" &nbsp; ");
+
+                }
+                else {
+                userGuessedVisisbleWord.push("_");
+                }
+
+            }
+
+            //remove commas
+            userGuessedVisisbleWord = userGuessedVisisbleWord.toString().replace(/,/g," " );
+
+            console.log("Selected word array " + wordArray + " array for user " + userGuessedVisisbleWord);
+
+            document.querySelector("#currentWord").innerHTML = "<h5>" + userGuessedVisisbleWord + "</h5>";
+            document.querySelector("#totalWins").innerHTML = "<h5>" + wins + "</h5>";
+            document.querySelector("#remainingGuesses").innerHTML = "<h5>" + guessesLeft + "</h5>";
+
+
+        }
+
+
         document.onkeyup = function(event) {
             //get the usersGuess
             var userGuess = event.key;
 
-            if(computerGuess.includes(userGuess)){
-                console.log("i guessed right");
+
+            for(var i = 0; i < wordArray.length; i++)
+            {
+                if(wordArray[i] === userGuess){
+                    alert("You got a letter");
+
+                
+                }
+                else{
+                    guessesLeft --;
+                    guessedLetters.push(userGuess);
+                }
 
 
             }
-            else{
-                console.log("i guessed wrong");
-
-            }
-
-            //get computers guess
-           // var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-            // if(userGuess === computerGuess)
-            // {
-            //     wins++;
-            //     guessesLeft = 10;
-            //     guessedLetters = [];
-            // }
-            // else{
-            //     if(guessesLeft === 0){
-            //         losses++;
-            //         guessesLeft = 10;
-            //         guessedLetters = [];
-            //     }
-            //     else{
-            //     guessesLeft--;
-            //     guessedLetters.push(userGuess);
-            //      }
-
-            // }
-
-
-            // html =
-            // "<h2> Press any key to get started </h2> "
-            // "<p> " + computerGuess + "</p>";
-       
-        document.querySelector("#game").innerHTML = html;
-
-
 
 
 
         };
-            
+    }
         
     
     
